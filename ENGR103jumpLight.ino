9a65a921c7354f2c8047c6db1929461fef5fccce
rWhitney311 <whitneyr@oregonstate.edu>
2024-06-05 13:56:15 -0700

    Update ENGR103jumpLight.ino

--- ENGR103jumpLight.ino
+++ ENGR103jumpLight.ino
@@ -1,56 +1,36 @@
 #include <Adafruit_CircuitPlayground.h>
+#include <AsyncDelay.h>
 
-float X, Y, Z, forceLevel;
- 
 void setup()
 {
-  Serial.begin(9600);
-
-  CircuitPlayground.begin();
-  CircuitPlayground.setAccelRange(LIS3DH_RANGE_8_G);
+Serial.begin(9600)
+CircuitPlayground.begin();
+attachInterrupt(digitalPinToInterrupt(7), blink, CHANGE);
+CircuitPlayground.slideSwitch();
+gameTimer.start(3000, AsyncDelay::MILLIS);
+// include jumpTimer
+lightString = (1,2,3,4)
+lightSpeed = slow=, med=, fast= in ms)
+  slow =
+  med =
+  fast = 
 }
 
 void loop()
 {
-  // OrangeRed ref: https://www.webmonkey.com/color-codes/
-  CircuitPlayground.clearPixels();
-  CircuitPlayground.setPixelColor(0, 255, 69, 0); // OrangeRed
-  CircuitPlayground.setPixelColor(1, 255, 69, 0);
-  CircuitPlayground.setPixelColor(2, 255, 69, 0);
-  CircuitPlayground.setPixelColor(3, 255, 69, 0);
-  CircuitPlayground.setPixelColor(4, 255, 69, 0);
-  CircuitPlayground.setPixelColor(5, 255, 69, 0);    
-  CircuitPlayground.setPixelColor(6, 255, 69, 0);
-  CircuitPlayground.setPixelColor(7, 255, 69, 0);
-  CircuitPlayground.setPixelColor(8, 255, 69, 0);
-  CircuitPlayground.setPixelColor(9, 255, 69, 0);
-
-
-  X = 0;
-  Y = 0;
-  Z = 0;
-  for (int i=0; i<10; i++) {
-    X += CircuitPlayground.motionX();
-    Y += CircuitPlayground.motionY();
-    Z += CircuitPlayground.motionZ();
-    delay(1);
+  if (switchFlag=TRUE)
+  {
+    lightString = 1
+    lightSpeed = slow
   }
-  X /= 10;
-  Y /= 10;
-  Z /= 10;
-
-  forceLevel = sqrt(X*X + Y*Y + Z*Z);
-
-  Serial.println(forceLevel);
-
-
-
-  if (forceLevel > 10)
-    CircuitPlayground.setBrightness(40);
-  else if (forceLevel >20)
-    CircuitPlayground.setBrightness(80);
   else
-    CircuitPlayground.setBrightness(10);
+  {
 
-delay(50);
+  }
+
+// primary game element
+  if digitalRead(LED8) // if LED8 is on
+  {
+    jumpTimer.start(400, AsyncDelay::MILLIS);
+  }
 }
